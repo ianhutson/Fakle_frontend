c677ae15f87d899aaf1b90dc71f0a3afa3992092
ianhutson <ih00209@georgiasouthern.edu>
2021-02-08 15:27:26 -0500

    started adding components

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './title';
+import Header from "./header";
+import Footer from "./footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Title />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <h1>This is header</h1>
+    </div>
+  );
+}
+export default Header;
