bac37143e24cd5f71a923e9cdde8ea0e0f12ec33
Ian Hutson <ih00209@georgiasouthern.edu>
2021-02-28 12:12:52 -0500

    leaderboard now sorted

--- src/components/ui/routes/ScoreList.js
+++ src/components/ui/routes/ScoreList.js
@@ -4,12 +4,19 @@ class ScoreList extends React.Component {
   
   scoreList = () => {
   const winnersArr = this.props.scores.map(score => score.winner.toLowerCase() )
-  const winnersTallies = []
+  var reduced = winnersArr.reduce( function (obj, val) {
+    obj[val] = (obj[val] || 0) + 1;
+    return obj;
+    }, {} );
+    var sorted = Object.keys(reduced).sort( function(a,b) {  
+    return reduced[b] - reduced[a];
+});
+console.log(sorted)
   const html = []
-  winnersArr.forEach(winner => {
-  if (winnersTallies.includes(winner) === false) winnersTallies.push(winner) 
+  sorted.forEach(winner => {
+  if (sorted.includes(winner) === false) sorted.push(winner) 
   });
-  winnersTallies.forEach(winner => {
+  sorted.forEach(winner => {
     html.push(<h1>{winner} - {winnersArr.filter(x => x === winner).length}</h1>)
   })
   return html
