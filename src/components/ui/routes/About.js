978927720ffafdd342bab54464a167391b72074c
ianhutson <ih00209@georgiasouthern.edu>
2021-03-02 12:24:00 -0500

    heroku

--- src/components/ui/routes/About.js
+++ src/components/ui/routes/About.js
@@ -7,10 +7,10 @@ function About() {
   return (
     <div>
     <Header/>
-<div className="pregame_container" style={{height: 'fit-content', width:'fit-content', fontFamily: 'sans-serif'}}>
-<h1>This app was developed with React as a portfolio project for Flatiron School by Ian Hutson in 2021.</h1>
-<br></br>
-<h1>Click "Rules" to learn how to play, or return to game.</h1>
+<div className="pregame_container" style={{height: 'fit-content', width:'50%', fontFamily: 'sans-serif', fontSize: '10px'}}>
+<h1 style={{textAlign:'left'}}>This app is a 'Farkle' clone developed with React as a portfolio project for Flatiron School by Ian Hutson in 2021. It was created to demonstrate the understaing and ability to use React, Redux, JavaScript ES6, Rails API, asynchronous actions, data persistence to a database, CSS, HTML, and client-side routing.</h1>
+<br></br><br></br>
+<h1 style={{textAlign:'left'}}>Click "Rules" to learn how to play, or return to game.</h1>
     <br></br>
     <HomeButton style={{}}/>
 </div>
