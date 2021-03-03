789693e178d0169980dce4c7c1591bff6b7e7377
ianhutson <ih00209@georgiasouthern.edu>
2021-03-03 17:23:13 -0500

    heroku deploy

--- src/components/actions/apiActions.js
+++ src/components/actions/apiActions.js
@@ -1,7 +1,7 @@
 export const fetchScores = () => {
     return (dispatch) => {
       dispatch({ type: 'LOADING'})
-      fetch('http://localhost:3001/games').then(response => {
+      fetch('http://fakle-backend.herokuapp.com/games').then(response => {
         return response.json()
         // return response.json()
       }).then(responseJSON => {
